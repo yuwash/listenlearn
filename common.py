af66e3307ee0a6337619d97a5464eca5f897553b
Yushin Washio <dev@yuwash.eu>
2026-01-04 01:27:57 +0100

    feat: Add playback orders to LearningMode

--- common.py
+++ common.py
@@ -1,4 +1,6 @@
 import dataclasses
+import enum
+import typing
 
 
 @dataclasses.dataclass(frozen=True)
@@ -10,7 +12,26 @@ class TTSParams:
     pitch_delta_hz: float = 0.0
 
 
+class PlaybackItem(enum.StrEnum):
+    Target = "target"
+    Fallback = "fallback"
+
+
 @dataclasses.dataclass(frozen=True)
 class LearningMode:
     target_params: TTSParams
     fallback_params: TTSParams
+    initial_playback_order: typing.Sequence[PlaybackItem] = (
+        PlaybackItem.Target,
+        PlaybackItem.Fallback,
+        PlaybackItem.Target,
+        PlaybackItem.Target,
+    )
+    short_target_review_playback_order: typing.Sequence[PlaybackItem] = (
+        PlaybackItem.Target,
+        PlaybackItem.Fallback,
+        PlaybackItem.Target,
+    )
+    long_target_review_playback_order: typing.Sequence[PlaybackItem] = (
+        PlaybackItem.Target,
+    )
